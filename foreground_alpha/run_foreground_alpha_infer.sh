e0267dc5f219bc98df7514c96c44b777fc3cac63
EntropyDrop <fake_email@entropydrop.com>
2026-07-01 14:51:48 +0800

    update

--- foreground_alpha/run_foreground_alpha_infer.sh
+++ foreground_alpha/run_foreground_alpha_infer.sh
@@ -1,6 +1,6 @@
 python ./infer.py \
   --checkpoint ./runs/foreground_alpha_test1/best.pt \
-  --inputs ../test_imgs/banana_front.png ../test_imgs/banana_back.png \
+  --inputs ../test_imgs/banana_output1_front.png ../test_imgs/banana_output1_back.png \
   --output_dir ../test_imgs
   ##--bg_color 0,0,0 \
   ##--uncompose
