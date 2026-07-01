f8d1af52bc9120e85d3a1c9bafcb3ba50f8bc65a
EntropyDrop <fake_email@entropydrop.com>
2026-07-02 02:47:39 +0800

    update

--- inverse_uv/run_inverse_uv_infer.sh
+++ inverse_uv/run_inverse_uv_infer.sh
@@ -1,7 +1,7 @@
 python3 ./infer.py \
-  --checkpoint ./runs/inverse_uv_test14/best.pt \
+  --checkpoint ./runs/inverse_uv_test17/best.pt \
   --output ../test_imgs/output.png \
   --mappings_dir ../../github/differentiable_minecraft_renderer/mappings \
-  --view_images ../test_imgs/banana_output1_front_rgba.png ../test_imgs/banana_output1_back_rgba.png 
-  #--view_images ../test_imgs/banana_front_rgba.png ../test_imgs/banana_back_rgba.png 
+  --view_images ../test_imgs/banana_front_rgba.png ../test_imgs/banana_back_rgba.png 
+  #--view_images ../test_imgs/banana_output1_front_rgba.png ../test_imgs/banana_output1_back_rgba.png 
   #--view_images ../test_imgs/front.png ../test_imgs/back.png 
--- inverse_uv/run_inverse_uv_training.sh
+++ inverse_uv/run_inverse_uv_training.sh
@@ -1,9 +1,15 @@
+
+# 1. 限制 PyTorch 的 CPU 并行线程，防止 256 线程严重锁竞争
+export OMP_NUM_THREADS=16
+export MKL_NUM_THREADS=16
+
 python train.py \
   --data_dir ../skins \
-  --max_samples 1000 \
-  --output_dir runs/inverse_uv_test14 \
+  --max_samples 10000 \
+  --output_dir runs/inverse_uv_test17 \
   --views walk_front_both_layer_ortho,walk_back_both_layer_ortho \
   --batch_size 16 \
+  --num_workers 16 \
   --epochs 100 \
   --val_split 0.1 \
   --mappings_dir ../../github/differentiable_minecraft_renderer/mappings \
