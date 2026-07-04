275bbe8a9bc81ba62fe3156f64a3cf8bbb9881d1
EntropyDrop <fake_email@entropydrop.com>
2026-07-04 20:06:51 +0800

    update

--- inverse_uv/run_inverse_uv_infer.sh
+++ inverse_uv/run_inverse_uv_infer.sh
@@ -25,7 +25,7 @@ python3 ./infer.py \
   --checkpoint "$CHECKPOINT" \
   --output ../test_imgs/output.png \
   --mappings_dir ../../github/differentiable_minecraft_renderer/mappings \
-  --view_images ../test_imgs/banana_output3_front_rgba.png ../test_imgs/banana_output3_back_rgba.png 
+  --view_images ../test_imgs/front_rgba.png ../test_imgs/back_rgba.png 
   #--view_images ../test_imgs/walk_front_both_layer_ortho_pyvista.png ../test_imgs/walk_back_both_layer_ortho_pyvista.png
   #--view_images ../test_imgs/banana_front_rgba.png ../test_imgs/banana_back_rgba.png
   #--view_images ../test_imgs/front.png ../test_imgs/back.png 
