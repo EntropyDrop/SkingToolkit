ec6b739e10c7be0b8eee624464324a1791f31fd0
EntropyDrop <fake_email@entropydrop.com>
2026-07-05 17:59:41 +0800

    update

--- foreground_alpha/run_foreground_alpha_infer.sh
+++ foreground_alpha/run_foreground_alpha_infer.sh
@@ -3,5 +3,5 @@ python ./infer.py \
   --inputs ../../SkingDataset/DDJ_real2render/test_output/img4_template7.png \
   --output_front ../test_imgs/front_rgba.png \
   --output_back ../test_imgs/back_rgba.png \
-  --bg_threshold 0.15 \
+  --bg_threshold 0.9 \
   --fill_holes
